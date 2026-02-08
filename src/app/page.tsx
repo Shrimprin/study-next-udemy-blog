e63f4b2127886b532bd1ebd39421856f47d00fc0
Kuroneko <ken.aster8@gmail.com>
2026-02-08 16:16:20 +0900

    必要なライブラリをインストール

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,33 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="
-      grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans
-      sm:p-20
-    ">
-      <main className="
-        row-start-2 flex flex-col items-center gap-[32px]
-        sm:items-start
-      ">
+    <div
+      className="
+        grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans
+        sm:p-20
+      "
+    >
+      <main
+        className="
+          row-start-2 flex flex-col items-center gap-[32px]
+          sm:items-start
+        "
+      >
         <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
-        <ol className="
-          list-inside list-decimal text-center font-mono text-sm/6
-          sm:text-left
-        ">
+        <ol
+          className="
+            list-inside list-decimal text-center font-mono text-sm/6
+            sm:text-left
+          "
+        >
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{' '}
-            <code className="
-              rounded-sm bg-black/5 px-1 py-0.5 font-mono font-semibold
-              dark:bg-white/6
-            ">
+            <code
+              className="
+                rounded-sm bg-black/5 px-1 py-0.5 font-mono font-semibold
+                dark:bg-white/6
+              "
+            >
               src/app/page.tsx
             </code>
             .
@@ -28,10 +36,12 @@ export default function Home() {
           <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
         </ol>
 
-        <div className="
-          flex flex-col items-center gap-4
-          sm:flex-row
-        ">
+        <div
+          className="
+            flex flex-col items-center gap-4
+            sm:flex-row
+          "
+        >
           <a
             className="
               flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent
