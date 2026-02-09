6b1d7197c7fd272b1ab77b661563b14d9ae46f83
Kuroneko <ken.aster8@gmail.com>
2026-02-09 21:06:43 +0900

    仮

---
+++ src/auth.ts
@@ -0,0 +1,31 @@
+import { authConfig } from '@/auth.config';
+import { prisma } from '@/lib/prisma';
+import bcrypt from 'bcryptjs';
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+
+async function getUser(email: string) {
+  return await prisma.user.findUnique({ where: { email } });
+}
+
+export const { auth, signIn, signOut, handlers } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(8) })
+          .safeParse(credentials);
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
+        }
+        return null;
+      },
+    }),
+  ],
+});
